1baa20f415a6543b90a4d2785adeafb68c92a5d7
wilgru <william.gruszka@gmail.com>
2022-07-17 22:24:56 +1000

    added dev route

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// if not production, 
+// if not production, serve 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
