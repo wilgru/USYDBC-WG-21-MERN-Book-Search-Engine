b22dd6310a30a8c5ff81dfd9eb0e94d548b3dd1d
wilgru <william.gruszka@gmail.com>
2022-07-19 16:16:59 +1000

    removed unused code

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -71,13 +71,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const { data } = await saveBook({
+      await saveBook({
         variables: { ...bookToSave }
       });
-
-      if (!data) {
-        throw new Error('something went wrong!');
-      }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
